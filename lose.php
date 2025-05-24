c4734eb9dd6ac6dac72ba7fa1af778cd5a97054e
DannyM18 <9018794@student.zadkine.nl>
2025-05-24 13:33:58 +0200

    Win - Lose screen
    Win & Lose

---
+++ lose.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Escape Room - You Lose!</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <h1>Helaas verloren!</h1>
+
+  <main>
+    <div class="info-box" style="text-align: center;">
+      <h2>Je hebt de Escape Room helaas niet optijd gehaald!</h2>
+      <p>Je was niet snel genoeg! Wil je nog een keer proberen?</p>
+    </div>
+
+    <a href="index.php" class="btn">Terug naar startpagina</a>
+
+  </main>
+
+  <footer>
+    Gemaakt door Gracjan en Danny
+  </footer>
+
+</body>
+
+</html>
---
+++ win.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Escape Room - You Win!</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <h1>Gefeliciteerd!</h1>
+
+  <main>
+    <div class="info-box" style="text-align: center;">
+      <h2>Je hebt de Escape Room succesvol voltooid!</h2>
+      <p>Wat een prestatie! Bedankt voor het spelen. Wil je nog een keer proberen?</p>
+    </div>
+
+    <a href="index.php" class="btn">Terug naar startpagina</a>
+
+  </main>
+
+  <footer>
+    Gemaakt door Gracjan en Danny
+  </footer>
+
+</body>
+
+</html>
